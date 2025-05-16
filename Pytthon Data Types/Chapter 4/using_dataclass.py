22cf6e77a7ebabec4d07122c1d936b394e717cbb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-05-16 16:56:26 +0600

    Register App User cls

--- Pytthon Data Types/Chapter 4/using_dataclass.py
+++ Pytthon Data Types/Chapter 4/using_dataclass.py
@@ -20,15 +20,11 @@ weight_log = [('Gentoo', 230.0, 5500.0, 'MALE'),
               ('Gentoo', 210.0, 4300.0, 'FEMALE'),
               ('Gentoo', 216.0, 4925.0, 'MALE')]
 
-# Create the empty list: labeled_entries
 labeled_entries = []
 
-# Iterate over the weight_log entries
 for species, flipper_length, body_mass, sex in weight_log:
-    # Append a new WeightEntry instance to labeled_entries
     labeled_entries.append(WeightEntry(
         species, flipper_length, body_mass, sex))
 
-# Print a list of the first 5 mass_to_flipper_length_ratio values
 print([entry.mass_to_flipper_length_ratio
       for entry in labeled_entries[:5]])

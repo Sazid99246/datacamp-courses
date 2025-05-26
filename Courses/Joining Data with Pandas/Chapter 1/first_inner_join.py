55619779dfcae9ef820978737b9165b9e158d20b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-05-26 09:50:10 +0600

    Joining Data with Pandas Chapter 1 and Chapter 2 completed

---
+++ Courses/Joining Data with Pandas/Chapter 1/first_inner_join.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+taxi_owners = pd.read_csv("taxi_owners.csv")
+taxi_veh = pd.read_csv("taxi_veh.csv")
+
+# Merge the taxi_owners and taxi_veh tables setting a suffix
+taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
+
+# Print the value_counts to find the most popular fuel_type
+print(taxi_own_veh['fuel_type'].value_counts())
---
+++ Courses/Joining Data with Pandas/Chapter 1/wards_census.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+wards = pd.read_csv("wards.csv")
+census = pd.read_csv("census.csv")
+wards_altered = pd.read_csv("wards_altered.csv")
+census_altered = pd.read_csv("census_altered.csv")
+
+# Merge the wards and census tables on the ward column
+wards_census = wards.merge(census, on="ward")
+
+# Print the shape of wards_census
+print('wards_census table shape:', wards_census.shape)
+
+# Print the first few rows of the wards_altered table to view the change 
+print(wards_altered[['ward']].head())
+
+# Merge the wards_altered and census tables on the ward column
+wards_altered_census = wards_altered.merge(census, on="ward")
+
+# Print the shape of wards_altered_census
+print('wards_altered_census table shape:', wards_altered_census.shape)
+
+
+# Print the first few rows of the census_altered table to view the change 
+print(census_altered[['ward']].head())
+
+# Merge the wards and census_altered tables on the ward column
+wards_census_altered = wards.merge(census_altered, on="ward")
+
+# Print the shape of wards_census_altered
+print('wards_census_altered table shape:', wards_census_altered.shape)
